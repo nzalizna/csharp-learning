0a5fa646ca548eaed0417a42bc5a727507b417d5
Nataliia Zalizna <zaliznaya.n@gmail.com>
2026-04-30 12:36:56 +0200

    Dodano Program1 - imie, wiek i rok urodzenia

---
+++ Program1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Podaj swoje imie");
+string imie = Console.ReadLine();
+Console.WriteLine("Podaj swój wiek");
+int wiek = int.Parse(Console.ReadLine());
+int rokUrodzenia = DateTime.UtcNow.Year - wiek;
+string pelneZdanie = "Twoje imie to " + imie + "\nTwoj wiek to " + wiek;
+Console.WriteLine(pelneZdanie);
+Console.WriteLine("Urodziles sie pewnie w roku " + rokUrodzenia);
+Console.ReadLine();
--- Spotkanie2/Spotkanie2/Program.cs
+++ Spotkanie2/Spotkanie2/Program.cs
@@ -103,4 +103,15 @@ Console.WriteLine(zdanie2);
 double malaLiczba = 1.999;
 double zaokraglona = Math.Round(malaLiczba, 0);
 Console.WriteLine(zaokraglona);
+
+
+
+Console.WriteLine("Podaj swoje imie");
+string imie = Console.ReadLine();
+Console.WriteLine("Podaj swój wiek");
+int wiek = int.Parse(Console.ReadLine());
+int rokUrodzenia = DateTime.UtcNow.Year - wiek;
+string pelneZdanie = "Twoje imie to " + imie + "\nTwoj wiek to " + wiek;
+Console.WriteLine(pelneZdanie);
+Console.WriteLine("Urodziles sie pewnie w roku " + rokUrodzenia);
 Console.ReadLine();
