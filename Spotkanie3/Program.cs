675a7976fa0b485d6ff27e926844d1fd771f2e1f
Nataliia Zalizna <zaliznaya.n@gmail.com>
2026-05-03 09:16:15 +0200

    Dodano metody — ObliczSrednia

--- Spotkanie3/Program.cs
+++ Spotkanie3/Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Metody / Методи
+double[] oceny = new double[] { 4, 5, 2, 3, 2 };
+double srednia = ObliczSrednia(oceny);
+Console.WriteLine(srednia);
+
+// Definicja metody / Визначення методу
+double ObliczSrednia(double[] oceny)
+{
+    double suma = 0;
+    foreach (double d in oceny)
+    {
+        suma += d;
+    }
+    return suma / oceny.Length;
+}
+
+double[] oceny2 = new double[] { 5, 4, 2, 4, 2, 3, 4 };
+double srednia2 = ObliczSrednia(oceny2);
+Console.WriteLine(srednia2);
