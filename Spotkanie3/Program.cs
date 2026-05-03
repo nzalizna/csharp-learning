32f04d077884314ab2ed4c8d6a57c0f41c4e5bcb
Nataliia Zalizna <zaliznaya.n@gmail.com>
2026-05-03 10:08:33 +0200

    Dodano klase Student z metodami — OOP

--- Spotkanie3/Program.cs
+++ Spotkanie3/Program.cs
@@ -1,4 +1,6 @@
 ﻿// Metody / Методи
+using Spotkanie3;
+
 double[] oceny = new double[] { 4, 5, 2, 3, 2 };
 double srednia = ObliczSrednia(oceny);
 Console.WriteLine(srednia);
@@ -17,3 +19,26 @@ double ObliczSrednia(double[] oceny)
 double[] oceny2 = new double[] { 5, 4, 2, 4, 2, 3, 4 };
 double srednia2 = ObliczSrednia(oceny2);
 Console.WriteLine(srednia2);
+
+// Obiekty / Об'єкти
+// new Student() = створюємо новий об'єкт за шаблоном класу Student
+Student st = new Student();
+st.Id = 1;
+st.Imie = "Jan";
+st.Nazwisko = "Kowalski";
+st.Oceny = new double[] { 4, 5, 2, 3, 2 };
+
+Student st2 = new Student();
+st2.Id = 2;
+st2.Imie = "Jana";
+st2.Nazwisko = "Rowalska";
+st2.Oceny = new double[] {3, 4, 3, 5, 4};
+
+// Wywołanie metod / Виклик методів
+st.PrzedstawSie();
+double srednia3 = st.ObliczSrednia();
+Console.WriteLine(srednia3);
+
+st2.PrzedstawSie();
+double srednia4 = st2.ObliczSrednia();
+Console.WriteLine(srednia4);
---
+++ Spotkanie3/Student.cs
@@ -0,0 +1,29 @@
+﻿namespace Spotkanie3;
+
+public class Student
+{
+    // Dane / Дані -- що описує студента
+    public int Id { get; set; }
+    public string Imie { get; set; }
+    public string Nazwisko { get; set; }
+    public string Email { get; set; }
+    public double[] Oceny { get; set; }
+
+
+// Zachowanie / Поведінка -- що студент вміє робити
+public void PrzedstawSie()
+    {
+        Console.WriteLine(Imie);
+        Console.WriteLine(Nazwisko);
+    }
+
+    public double ObliczSrednia()
+    {
+        double suma = 0;
+        foreach (double d in Oceny)
+        {
+            suma += d;
+        }
+        return suma / Oceny.Length;
+    }
+}
